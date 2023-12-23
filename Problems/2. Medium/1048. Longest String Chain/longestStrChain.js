0465673f6fa778c02fb10d0205ebb4f8227f2abb
Kay Nguyen <kaynguyen.work@gmail.com>
2023-12-23 20:04:50 +0700

    sync

---
+++ Problems/2. Medium/1048. Longest String Chain/longestStrChain.js
@@ -0,0 +1,10 @@
+var longestStrChain = function(words) {
+    let result = 0;
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            
+        }        
+    }
+};
+words = ["a","b","ba","bca","bda","bdca"]
+console.log(longestStrChain(words))
