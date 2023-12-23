3e727e53a0e468a20fbc096e97643a774edaf426
Kay Nguyen <kaynguyen.work@gmail.com>
2023-12-23 23:19:14 +0700

    add prettier config

--- Problems/1. Easy/1995. Count Special Quadruplets/countQuadruplets.js
+++ Problems/1. Easy/1995. Count Special Quadruplets/countQuadruplets.js
@@ -8,50 +8,43 @@ var countQuadruplets = function (nums) {
     let c = 2;
     let d = 3;
     let result = 0;
-    // let q = 0;
     while (10) {
-        // q++;
-        // console.log(a, b, c, d);
         if (nums[a] + nums[b] + nums[c] == nums[d]) {
             result++;
         }
-        const cacheA = a;
-        const cacheB = b;
-        const cacheC = c;
-        const cacheD = d;
         if (
-            cacheA == nums.length - 4 &&
-            cacheB == nums.length - 3 &&
-            cacheC == nums.length - 2 &&
-            cacheD == nums.length - 1
+            a == nums.length - 4 &&
+            b == nums.length - 3 &&
+            c == nums.length - 2 &&
+            d == nums.length - 1
         ) {
             break;
         }
         if (
-            cacheB == nums.length - 3 &&
-            cacheC == nums.length - 2 &&
-            cacheD == nums.length - 1
+            b == nums.length - 3 &&
+            c == nums.length - 2 &&
+            d == nums.length - 1
         ) {
-            a = cacheA + 1;
+            a = a + 1;
             b = a + 1;
             c = b + 1;
             d = c + 1;
-            continue
+            continue;
         }
-        if (cacheC == nums.length - 2 && cacheD == nums.length - 1) {
-            b = cacheB + 1;
+        if (c == nums.length - 2 && d == nums.length - 1) {
+            b = b + 1;
             c = b + 1;
             d = c + 1;
-            continue
+            continue;
         }
-        if (cacheD == nums.length - 1) {
-            c = cacheC + 1;
+        if (d == nums.length - 1) {
+            c = c + 1;
             d = c + 1;
-            continue
+            continue;
         } else d++;
     }
     return result;
 };
 
 nums = [1, 1, 1, 3, 5];
-// console.log(countQuadruplets(nums));
+console.log(countQuadruplets(nums));
--- test2.js
+++
@@ -1,3 +0,0 @@
-var dieSimulator = function (n, rollMax) {};
-(n = 2), (rollMax = [1, 1, 2, 2, 2, 3]);
-console.log(dieSimulator(n, rollMax));
