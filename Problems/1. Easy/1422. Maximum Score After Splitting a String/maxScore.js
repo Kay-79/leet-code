6d981a341ad0e8171fb8b9e0e20b9ab29e26eb46
Kay Nguyen <kaynguyen.work@gmail.com>
2023-12-24 14:36:37 +0700

    done 1422

---
+++ Problems/1. Easy/1422. Maximum Score After Splitting a String/maxScore.js
@@ -0,0 +1,27 @@
+const maxScore = (t) => {
+    const s = t.split("");
+    let indexSplit = 1;
+    let result = 0;
+    while (indexSplit < s.length) {
+        let countZero = 0;
+        let countOne = 0;
+        for (let i = 0; i < s.length; i++) {
+            if (i < indexSplit) {
+                if (s[i] == "0") {
+                    countZero++;
+                }
+            } else {
+                if (s[i] == "1") {
+                    countZero++;
+                }
+            }
+        }
+        countOne + countZero > result
+            ? (result = countOne + countZero)
+            : (result = result);
+        indexSplit++;
+    }
+    return result;
+};
+s = "00";
+console.log(maxScore(s));
