a08064e90ef67371b614f96b107d1d130ebc4e93
Kay Nguyen <kaynguyen.work@gmail.com>
2025-01-09 14:09:30 +0700

    2255. Count Prefixes of a Given String

---
+++ Problems/Solved/Easy/2255.ts
@@ -0,0 +1,13 @@
+describe("Typescript test:", () => {
+    test("Testing", () => {
+        expect(countPrefixes(["a","b","c","ab","bc","abc"], "abc")).toEqual(3);
+    });
+});
+
+function countPrefixes(words: string[], s: string): number {
+    let result = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (s.startsWith(words[i])) result++;
+    }
+    return result;
+}
