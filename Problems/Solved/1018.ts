64ab31b5416108250ba32e660427702f70524553
Kay Nguyen <kaynguyen.work@gmail.com>
2025-11-24 14:49:51 +0700

    1018. Binary Prefix Divisible By 5

---
+++ Problems/Solved/1018.ts
@@ -0,0 +1,79 @@
+describe("Typescript test:", () => {
+    test("Testing", () => {
+        const nums = [
+            1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1,
+            0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1,
+        ];
+        expect(prefixesDivBy5(nums)).toEqual([
+            false,
+            false,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            true,
+            false,
+            false,
+            false,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+        ]);
+    });
+});
+
+function prefixesDivBy5(nums: number[]): boolean[] {
+    const res: boolean[] = [];
+    let rem = 0;
+    for (const bit of nums) {
+        rem = ((rem << 1) + bit) % 5;
+        res.push(rem === 0);
+    }
+    return res;
+}
