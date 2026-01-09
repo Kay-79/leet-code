b63848d038c8ddedc4d1b4a6c91907d479a22247
Kay Nguyen <kaynguyen.work@gmail.com>
2026-01-09 14:48:53 +0700

    1123. Lowest Common Ancestor of Deepest Leaves

---
+++ Problems/1123.rs
@@ -0,0 +1,42 @@
+pub mod utils;
+use std::cell::RefCell;
+use std::rc::Rc;
+use utils::tree::TreeNode;
+pub struct Solution;
+
+impl Solution {
+    pub fn lca_deepest_leaves(
+        root: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::dfs(&root).1
+    }
+    pub fn dfs(node: &Option<Rc<RefCell<TreeNode>>>) -> (i32, Option<Rc<RefCell<TreeNode>>>) {
+        match node {
+            None => (0, None),
+            Some(n) => {
+                let n_ref = n.borrow();
+                let (r_deepth, r_ref) = Self::dfs(&n_ref.left);
+                let (l_deepth, l_ref) = Self::dfs(&n_ref.right);
+                if r_deepth > l_deepth {
+                    return (r_deepth + 1, r_ref);
+                } else if r_deepth < l_deepth {
+                    return (l_deepth + 1, l_ref);
+                }
+                return (l_deepth + 1, node.clone());
+            }
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case() {
+        let root = tree![3, 5, 1, 6, 2, 0, 8, null, null, 7, 4];
+        let expected_output = tree![2, 7, 4];
+        assert_eq!(Solution::lca_deepest_leaves(root), expected_output);
+    }
+}
+
+fn main() {}
